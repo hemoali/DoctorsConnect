6298a15ec1d2aa8bef9df8a899a559d3dc6c910a
Ibrahim Ali Radwan <i.radwan1996@gmail.com>
2016-08-22 17:17:43 +0300

    Fix docotrs list font size

--- app/src/main/java/ibrahim/radwan/doctorsconnect/ListViewFragment.java
+++ app/src/main/java/ibrahim/radwan/doctorsconnect/ListViewFragment.java
@@ -361,7 +361,7 @@ public class ListViewFragment extends Fragment {
                             View view = super.getView(position, convertView, parent);
                             TextView text1 = (TextView) view.findViewById(android.R.id.text1);
                             text1.setTextColor(Color.BLACK);
-                            text1.setTextSize(getResources().getDimension(R.dimen.normal_font_size));
+                            text1.setTextSize(getResources().getDimension(R.dimen.doctors_list_font_size));
                             return view;
                         }
                     };
